406945ff6526ae641ecdb7e128ce3aa00036c1da
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2023-03-02 09:20:44 +0530

    ➕ added models

---
+++ QuizApplication/DbContext/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using QuizApplication.Models;
+
+namespace QuizApplication.DbContext
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
