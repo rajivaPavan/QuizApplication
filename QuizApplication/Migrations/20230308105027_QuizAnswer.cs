03697e76160528d25a94869d603471d43fa3b86f
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2023-03-08 19:00:53 +0530

    ⚒️ working on quiz

---
+++ QuizApplication/Migrations/20230308105027_QuizAnswer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace QuizApplication.Migrations
+{
+    public partial class QuizAnswer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "QuestionNo",
+                table: "QuizQuestions",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartedAt",
+                table: "QuizQuestions",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "SubmittedAt",
+                table: "QuizQuestions",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "QuestionNo",
+                table: "QuizQuestions");
+
+            migrationBuilder.DropColumn(
+                name: "StartedAt",
+                table: "QuizQuestions");
+
+            migrationBuilder.DropColumn(
+                name: "SubmittedAt",
+                table: "QuizQuestions");
+        }
+    }
+}
